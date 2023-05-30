58154e3698c0b2a40d312d7969ab35cddd27522f
Lirovsk <joao.lira@engenharia.ufjf.br>
2023-05-30 17:39:05 -0300

    código morse

--- pinos.py
+++ pinos.py
@@ -4,8 +4,46 @@ import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
 while True:
+    #s
+    GPIO.output(12, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(12, GPIO.LOW)
+    time.sleep(0.5)
+    GPIO.output(12, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(12, GPIO.LOW)
+    time.sleep(0.5)
+    GPIO.output(12, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(12, GPIO.LOW)
+    time.sleep(1)
+    #o
     GPIO.output(12, GPIO.HIGH)
     time.sleep(1)
     GPIO.output(12, GPIO.LOW)
     time.sleep(1)
-    
+    GPIO.output(12, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(12, GPIO.LOW)
+    time.sleep(1)
+    GPIO.output(12, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(12, GPIO.LOW)
+    time.sleep(1)
+    #s
+    GPIO.output(12, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(12, GPIO.LOW)
+    time.sleep(0.5)
+    GPIO.output(12, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(12, GPIO.LOW)
+    time.sleep(0.5)
+    GPIO.output(12, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(12, GPIO.LOW)
+    time.sleep(2)
+
+
+
+
